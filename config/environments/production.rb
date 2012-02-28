5d716e75c22658bf9bf00b343f1c6bf3fe855d36
Carlos Garcia <kartalon@gmail.com>
2012-02-29 00:32:24 +0100

    Precompile blueprint css in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,8 @@ SampleApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Precompile Blueprint CSS
+  config.assets.precompile += [ "blueprint/*.css" ]
+
 end
